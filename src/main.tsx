7e72f70fb47cc4a0d0026ad814caea762329bad9
krantikumar-ch <krantikumar.ch@gmail.com>
2023-03-18 19:58:47 +0530

    built a nav bar

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import "./index.css";
 
-// 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-
-const theme = extendTheme({ colors });
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </React.StrictMode>
